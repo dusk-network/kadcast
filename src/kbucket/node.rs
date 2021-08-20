081e09c103a464a8bd4d11adae7077453b7c0e22
herr-seppia <herrseppia@gmail.com>
2021-08-20 11:04:31 +0200

    Better LRU Policy (#12)
    * fix #10
    handle pending node in a safer way
    added bucket configuration
    Introduced NodeEvictionStatus to handle eviction properly
    
    * fix #11 
    refactor bucket.insert signature to canonical Result
    
    * close #6 
    KBucket completed

--- src/kbucket/node.rs
+++ src/kbucket/node.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use crate::utils;
 
 use super::key::BinaryID;
@@ -5,11 +7,24 @@ use super::key::BinaryID;
 pub struct Node<TKey: BinaryID, TValue> {
     id: TKey,
     value: TValue,
+    pub(super) eviction_status: NodeEvictionStatus,
+    pub(super) seen_at: Instant,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum NodeEvictionStatus {
+    None,
+    Requested(Instant),
 }
 
 impl<TKey: BinaryID, TValue> Node<TKey, TValue> {
-    pub fn new(id: TKey, value: TValue) -> Node<TKey, TValue> {
-        Node { id, value }
+    pub fn new(id: TKey, value: TValue) -> Self {
+        Node {
+            id,
+            value,
+            seen_at: Instant::now(),
+            eviction_status: NodeEvictionStatus::None
+        }
     }
 
     pub fn calculate_distance(&self, other: &Node<TKey, TValue>) -> Option<usize> {
@@ -23,5 +38,18 @@ impl<TKey: BinaryID, TValue> Node<TKey, TValue> {
 
     pub fn id(&self) -> &TKey {
         &self.id
+    }
+
+    pub(super) fn refresh(&mut self) {
+        self.eviction_status=NodeEvictionStatus::None;
+        self.seen_at = Instant::now();
+    }
+
+    pub(super) fn flag_for_check(&mut self) {
+        self.eviction_status=NodeEvictionStatus::Requested(Instant::now());
+    }
+
+    pub(super) fn is_alive(&self, duration:Duration) -> bool {
+        self.seen_at.elapsed() < duration
     }
 }
