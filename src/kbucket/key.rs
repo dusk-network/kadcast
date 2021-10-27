fe3ab0e0c1c296467456cd5834e0cad249ed324b
Herr Seppia <seppia@dusk.network>
2021-10-27 15:27:49 +0200

    Include copyright and license in sources
    - Add `cargo-dusk-analyzer` check to CI
    - Change `rustfmt.toml` to force `newline_style`
    
    Resolves: #43

--- src/kbucket/key.rs
+++ src/kbucket/key.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
 use crate::encoding::Marshallable;
 use crate::K_ID_LEN_BYTES;
 use crate::K_NONCE_LEN;
