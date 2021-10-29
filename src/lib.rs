f4df640c0812c6d579b58650b868aeecd94ee773
Herr Seppia <seppia@dusk.network>
2021-10-29 16:01:10 +0200

    Document public API
    Resolves: #46

--- src/lib.rs
+++ src/lib.rs
@@ -34,14 +34,14 @@ const K_BETA: usize = 3;
 
 const K_CHUNK_SIZE: usize = 1024;
 
-/// Default value while which a node is considered alive (no eviction will be
+/// Default value while a node is considered alive (no eviction will be
 /// requested)
 pub const BUCKET_DEFAULT_NODE_TTL_MILLIS: u64 = 30000;
 
-/// Default value after while a node can be evicted if requested
+/// Default value after which a node can be evicted if requested
 pub const BUCKET_DEFAULT_NODE_EVICT_AFTER_MILLIS: u64 = 5000;
 
-/// Default value after while a bucket is considered idle
+/// Default value after which a bucket is considered idle
 pub const BUCKET_DEFAULT_TTL_SECS: u64 = 60 * 60;
 
 /// Struct representing the Kadcast Network
@@ -113,8 +113,7 @@ impl Server {
     ///
     /// Note:
     /// The function returns just after the message is put on the internal queue
-    /// system. It **does not guarantee** message is broadcasted in the
-    /// meanwhile
+    /// system. It **does not guarantee** the message will be broadcasted
     pub async fn broadcast(&self, message: &[u8]) {
         if message.is_empty() {
             return;
@@ -135,10 +134,10 @@ impl Server {
 
     /// Instantiate a [ServerBuilder].
     ///
-    /// * `public_ip` - String representing the public `SocketAddress` where
-    ///   `Server` is reachable. No domain name allowed.
+    /// * `public_ip` - public `SocketAddress` of the `Server`. No domain name
+    ///   allowed
     /// * `bootstrapping_nodes` - List of known bootstrapping kadcast nodes. It
-    ///   accepts same representation of `public_ip` but with domain names
+    ///   accepts the same representation of `public_ip` but with domain names
     ///   allowed
     /// * `on_message` - Function called each time a broadcasted message is
     ///   received from the network
@@ -151,10 +150,7 @@ impl Server {
     }
 }
 
-/// Kadcast [Server] builder
-///
-/// Builder for Kadcast [Server], allows to instantiate a [Server] and to tweak
-/// his parameters
+/// ServerBuilder instantiates a Kadcast [Server].
 pub struct ServerBuilder {
     node_ttl: Duration,
     node_evict_after: Duration,
@@ -165,8 +161,8 @@ pub struct ServerBuilder {
 }
 
 impl ServerBuilder {
-    /// Set duration while which a node is considered alive (no eviction will be
-    /// requested).
+    /// Sets the maximum duration for a node to be considered alive (no eviction
+    /// will be requested).
     ///
     /// Default value [BUCKET_DEFAULT_NODE_TTL_MILLIS]
     pub fn with_node_ttl(mut self, node_ttl: Duration) -> ServerBuilder {
@@ -174,7 +170,7 @@ impl ServerBuilder {
         self
     }
 
-    /// Set duration after while a bucket is considered idle
+    /// Set duration after which a bucket is considered idle
     ///
     /// Default value [BUCKET_DEFAULT_TTL_SECS]
     pub fn with_bucket_ttl(mut self, bucket_ttl: Duration) -> ServerBuilder {
@@ -182,7 +178,7 @@ impl ServerBuilder {
         self
     }
 
-    /// Set duration after while a node can be evicted if requested
+    /// Set duration after which a node can be evicted if requested
     ///
     /// Default value [BUCKET_DEFAULT_NODE_EVICT_AFTER_MILLIS]
     pub fn with_node_evict_after(
